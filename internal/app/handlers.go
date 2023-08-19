7ce3a13ee25a63247d002dfe16bf370d74fc7207
Leonid Sukhinin <leonsukh@gmail.com>
2023-08-20 00:23:56 +0300

    upd

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -45,6 +45,8 @@ type Metadata struct {
 
 func (a *Application) BuyHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 	type RequestParams struct {
 		Rate  int    `json:"rate"`
