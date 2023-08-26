4d287938c1bc7af0966ac413eb217e0ec3276a78
Leonid Sukhinin <leonsukh@gmail.com>
2023-08-26 18:02:36 +0300

    upd

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,4 +11,4 @@ const MaxSell3 int = 11
 var MaxSell = [3]int{MaxSell1, MaxSell2, MaxSell3}
 
 const StartSell string = "2023-08-18 15-00-00"
-const EndSell string = "2023-08-25 15-00-00"
+const EndSell string = "2023-08-27 15-00-00"
