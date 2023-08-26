4b4bc3f3a56e064a1cd6390e2fabecd17f5764d3
Leonid Sukhinin <leonsukh@gmail.com>
2023-08-26 18:13:05 +0300

    upd

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,8 @@
 package config
 
-const MaxSell1 int = 22
-const MaxSell2 int = 7
-const MaxSell3 int = 11
+const MaxSell1 int = 29
+const MaxSell2 int = 15
+const MaxSell3 int = 10
 
 //const MaxSell1 int = 0
 //const MaxSell2 int = 0
