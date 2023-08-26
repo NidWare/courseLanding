f77d19ac69ef07323acd5552cd454033701e2e4f
Leonid Sukhinin <leonsukh@gmail.com>
2023-08-26 16:49:49 +0300

    upd

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -81,7 +81,7 @@ func (a *Application) BuyHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, "Sold out", http.StatusMethodNotAllowed)
 			return
 		}
-		url, id, err = a.PaymentService.MakePayment(10.00, params.Name, params.Email, phone)
+		url, id, err = a.PaymentService.MakePayment(15000.00, params.Name, params.Email, phone)
 		if err != nil {
 			http.Error(w, "Problems with ukassa", http.StatusBadRequest)
 			return
